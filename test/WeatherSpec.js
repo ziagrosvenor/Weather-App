65bb0dc54edbbffc841798477344a1907da4158f
ziagrosvenor <ziagrosvenor@live.co.uk>
2014-12-12 17:39:13 +0000

    Adds unit test for weather data route.

--- test/WeatherSpec.js
+++ test/WeatherSpec.js
@@ -10,15 +10,27 @@ describe('weather', function () {
 		.expect(200)
 		.end( function (err, res) {
 			res.status.should.equal(200);
-			done();
+			done(); 
 		});
 	});
+
 	it('should return an error for invalid request', function (done) {
 		supertest(app)
 		.get('/foo')
 		.expect(404)
 		.end( function (err, res) {
 			res.status.should.equal(404);
+			done();
+		});
+	});
+
+	it('should return 250 records from the database', function (done) {
+		supertest(app)
+		.get('/api/weather/')
+		.expect(200)
+		.end( function (err, res) {
+			res.status.should.equal(200);
+			res.body.length.should.equal(250);
 			done();
 		});
 	});
